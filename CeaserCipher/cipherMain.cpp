63ef400a380c96e5afa29c68f44a6ae76046c2ca
Tjccs <tiago.jccs@protonmail.com>
2019-03-10 22:59:21 +0000

    Update the cipher

--- CeaserCipher/cipherMain.cpp
+++ CeaserCipher/cipherMain.cpp
@@ -5,96 +5,85 @@
 #include <algorithm>
 #include <fstream>
 #include <vector>
+#include <cassert>
+#include <iterator>
 
 
-std::vector<std::string> encode(const std::vector<std::string> &str, int shift)
+std::string encodeStr(const std::string& str, int shift)
+{
+	std::string str_enc;
+	std::transform(str.cbegin(), str.cend(), std::back_inserter(str_enc), [&](char ch) -> char
+	{
+		if (ch == 'z' || ch == 'Z')
+		{
+			return ch - 25;
+		}
+		else if (isspace(ch))
+		{
+			return ' ';
+		}
+		else
+		{
+			return ch + shift;
+		}
+	});
+
+	return str_enc;
+}
+
+std::vector<std::string> encodeVec(const std::vector<std::string>& strVector, int shift)
 {
 	std::vector<std::string> tempMsg;
+	tempMsg.reserve(strVector.size());
 
-	for (std::string lines : str)
-	{
-		// std::transform(lines.cbegin(), lines.cend(), std::back_inserter(tempMsg), [&](char ch) -> char
-		for (char &ch : lines)
-		{
-			if (ch == 'z' || ch == 'Z')
-			{
-				ch -= 25;
-			}
-			else if (isspace(ch)) {
-				ch = ' ';
-			}
-			else
-			{
-				ch += shift;
-			}
-		}
-		tempMsg.push_back(lines);
-	}
+	std::transform(strVector.cbegin(), strVector.cend(), std::back_inserter(tempMsg),
+		[&](const std::string& s) { return encodeStr(s, shift); });
 
 	return tempMsg;
 }
 
 std::vector<std::string> decode(const std::vector<std::string> &str, int shift)
 {
-	return encode(str, -1 * shift);
+	return encodeVec(str, -1 * shift);
 }
-
 
 int main(int argc, char *argv[])
 {
 	int choice;
 	std::cout << "What do you want to do? 1.Encrypt, 2.Decrypt: ";
 	std::cin >> choice;
-	
-	
+
+	if (!(choice == 1 || choice == 2)) {
+		return EXIT_FAILURE;
+	}
+
 	int key;
 	std::cout << "Enter desired shift: ";
 	std::cin >> key;
 
-
 	std::ifstream inFile("testfile.txt");
-	if (!(inFile.is_open()))
+	if (!inFile)
 	{
-		std::cout << "There was a problem with the file!";
-		std::exit(EXIT_FAILURE); // ExitProcess() if windows especific.
+		// std::cout << "There was a problem with the file!";
+		return EXIT_FAILURE; // ExitProcess() if windows especific.
 	}
-
-	std::vector<std::string> finalResult;
 
 	std::string line;
 	std::vector<std::string> lines;
-
 
 	while (std::getline(inFile, line))
 	{
 		lines.push_back(line);
 	}
-	inFile.close();
 
-	if (choice == 1)
+	const std::vector<std::string> finalResult = [&]()
 	{
-		auto result = encode(lines, key);
-		finalResult = result;
-	}
+		return choice == 1 ? encodeVec(lines, key) : decode(lines, key);
+	}();
 
-	else if (choice == 2)
-	{
-		auto result = decode(lines, key);
-		finalResult = result;
-	}
-	else
-	{
-		std::cout << "Wrong option, exiting!";
-		std::exit(EXIT_FAILURE);
-	}
-
-	
 	std::ofstream outFile("testfile.txt");
-	for (auto i = finalResult.begin(); i != finalResult.end(); ++i)
-	{
-		outFile << *i << '\n';
-	}
-	outFile.close();
+	std::copy(finalResult.cbegin(), finalResult.cend(),
+		std::ostream_iterator<std::string>(outFile, "\n"));
 
-	std::exit(EXIT_SUCCESS);
+	return EXIT_SUCCESS;
 }
